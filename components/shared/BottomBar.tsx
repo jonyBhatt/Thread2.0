6f4b76fb04b6cfa0593f701ff8e34a6d8550b581
Jony Bhattacharjee <jonybhattacharjee886@gmail.com>
2023-08-08 16:44:19 +0600

    Bottombar

--- components/shared/BottomBar.tsx
+++ components/shared/BottomBar.tsx
@@ -1,9 +1,33 @@
+"use client";
+import { sidebarLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 function BottomBar() {
-    return (
-        <section className="bottombar">
-            <div className="bottombar_container"></div>
-        </section>
-    )
+	// const router = useRouter();
+	const pathname = usePathname();
+	return (
+		<section className="bottombar">
+			<div className="bottombar_container">
+				{sidebarLinks.map((link) => {
+					const isActive =
+						(pathname.includes(link.route) && link.route.length > 1) ||
+						pathname === link.route;
+					return (
+						<Link
+							href={link.route}
+							key={link.label}
+							className={`bottombar_link ${
+								isActive ? "bg-primary-500" : ""
+							}`}>
+							<Image src={link.imgURL} alt="nav_menu" width={24} height={24} />
+							<p className="max-sm:hidden text-subtle-medium text-light-1">{link.label.split(/\s./)[0]}</p>
+						</Link>
+					);
+				})}
+			</div>
+		</section>
+	);
 }
 
 export default BottomBar;
